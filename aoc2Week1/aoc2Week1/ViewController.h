a8c257ce8833be5a0205e434d52f619278808517
Jimmy Kim <jimmykim925@fullsail.edu>
2012-09-24 17:10:56 -0700

    initial files

---
+++ aoc2Week1/aoc2Week1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  aoc2Week1
+//
+//  Created by Jimmy Kim on 9/24/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ aoc2Week1/aoc2Week1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  aoc2Week1
+//
+//  Created by Jimmy Kim on 9/24/12.
+//  Copyright (c) 2012 Full Sail University. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
